4a2e157eb156968e9cd90ae5e075d8727e86bd55
jccsISC <silva.jccs18@gmail.com>
2020-10-30 09:04:43 -0600

    se agrego la dependencia para utilizar el mapa en las vistas, un toolbar para cerrar sesion en los dos tipos de usuarios

--- app/src/main/java/com/jccsisc/myuberclone/activities/client/MapClientActivity.java
+++ app/src/main/java/com/jccsisc/myuberclone/activities/client/MapClientActivity.java
@@ -1,39 +1,70 @@
 package com.jccsisc.myuberclone.activities.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.jccsisc.myuberclone.R;
 import com.jccsisc.myuberclone.activities.MainActivity;
+import com.jccsisc.myuberclone.includes.MyToolbar;
 import com.jccsisc.myuberclone.providers.AuthProvider;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private Button btnLogout;
     AuthProvider mAuthProvider;
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
 
-        btnLogout = findViewById(R.id.btnLogout);
+        MyToolbar.showToolbar(MapClientActivity.this, "Cliente", false);
+
         mAuthProvider = new AuthProvider();
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuthProvider.logout();
+    }
 
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);//seleccionamos el tipo de mapa que vamos a usar
+    }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.client_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void logout() {
+        mAuthProvider.logout();
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/jccsisc/myuberclone/activities/driver/MapDriveActivity.java
+++ app/src/main/java/com/jccsisc/myuberclone/activities/driver/MapDriveActivity.java
@@ -1,17 +1,73 @@
 package com.jccsisc.myuberclone.activities.driver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.jccsisc.myuberclone.R;
+import com.jccsisc.myuberclone.activities.MainActivity;
+import com.jccsisc.myuberclone.includes.MyToolbar;
+import com.jccsisc.myuberclone.providers.AuthProvider;
 
-public class MapDriveActivity extends AppCompatActivity {
+public class MapDriveActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+
+    AuthProvider mAuthProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_drive);
 
+        MyToolbar.showToolbar(MapDriveActivity.this, "Conductor", false);
+
+        mAuthProvider = new AuthProvider();
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
+    }
+
+
+    //debemos de agregar la sig. propiedad al manifest dentro de aplication para que funcione la app
+    //<meta-data android:name="com.google.android.geo.API_KEY" android:value="@string/google_api_key"/>
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);//seleccionamos el tipo de mapa que vamos a usar
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void logout() {
+        mAuthProvider.logout();
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
